d425448e60b3881799642ee89d810f76fbe22012
Ermin Šabotić <ermin@misijaweb.com>
2017-06-12 09:04:03 +0200

    Edit User Info
    Edit User Info Added

--- ChatApp/Pages/HomePage.xaml.cs
+++ ChatApp/Pages/HomePage.xaml.cs
@@ -14,12 +14,15 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using ChatApp.Api;
 using ChatApp.Dialog;
 using ChatApp.Model;
+using ChatApp.Request;
 using ChatApp.ViewModel;
 using Refit;
+using User = Windows.System.User;
 
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -38,6 +41,16 @@ namespace ChatApp.Pages
             this.InitializeComponent();
             viewModel = (HomeViewModel)DataContext;
             SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            UserImage.Source =
+                new BitmapImage(new Uri("https://scontent.flju2-1.fna.fbcdn.net/v/t1.0-9/11391338_1064066726954436_6934638157053324450_n.jpg?oh=fb94393d33f67b53c582670081a956c4&oe=59A401EC", UriKind.RelativeOrAbsolute));
+            Username.Text = HttpApi.LoggedInUser.Username;
+            email.Text = HttpApi.LoggedInUser.Email;
+            company.Text = HttpApi.LoggedInUser.Company;
+            FirstName.Text = HttpApi.LoggedInUser.FirstName;
+            LastName.Text = HttpApi.LoggedInUser.LastName;
+            country.Text = HttpApi.LoggedInUser.Country;
+            DatePicker.Date = HttpApi.LoggedInUser.DateOfBirth;
+            gender.Text = HttpApi.LoggedInUser.Gender;
         }
 
 
@@ -84,6 +97,65 @@ namespace ChatApp.Pages
         private async void NewTeamButton_Click(object sender, RoutedEventArgs e)
         {
             await new AddTeamDialog(team => viewModel.Teams.Add(team)).ShowAsync();
+        }
+
+
+        private async void Edit_info(object sender, RoutedEventArgs e)
+        {
+            if ((string)Edit_UserInfo.Content == "Edit Info")
+            {
+                Username.IsEnabled = true;
+                company.IsEnabled = true;
+                FirstName.IsEnabled = true;
+                LastName.IsEnabled = true;
+                country.IsEnabled = true;
+                DatePicker.IsEnabled = true;
+                gender.IsEnabled = true;
+
+                Edit_UserInfo.Content = "Edit";
+            }
+            else
+            {
+                try
+                {
+                    var response = await HttpApi.User.EditInfoAsync(new EditUserInfoRequest()
+                    {
+                        Username = Username.Text,
+
+                        Company = company.Text,
+                        FirstName = FirstName.Text,
+                        LastName = LastName.Text,
+                        Country = country.Text,
+                        DateOfBirth = DatePicker.Date.DateTime,
+                        Gender = gender.Text
+                    }, HttpApi.AuthToken);
+
+                    HttpApi.LoggedInUser.Username = Username.Text;
+
+                    HttpApi.LoggedInUser.Company = company.Text;
+                    HttpApi.LoggedInUser.FirstName = FirstName.Text;
+                    HttpApi.LoggedInUser.LastName = LastName.Text;
+                    HttpApi.LoggedInUser.Country = country.Text;
+                    HttpApi.LoggedInUser.DateOfBirth = DatePicker.Date.DateTime;
+                    HttpApi.LoggedInUser.Gender = gender.Text;
+
+                    Username.IsEnabled = false;
+                    email.IsEnabled = false;
+                    company.IsEnabled = false;
+                    FirstName.IsEnabled = false;
+                    LastName.IsEnabled = false;
+                    country.IsEnabled = false;
+                    DatePicker.IsEnabled = false;
+                    gender.IsEnabled = false;
+
+                    Edit_UserInfo.Content = "Edit Info";
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+               
+            }
         }
     }
 }
