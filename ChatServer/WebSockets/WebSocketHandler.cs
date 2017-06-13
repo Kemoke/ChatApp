7cb14c7c58f0164c96c6ec2294b0516b54d5d34d
Kemal Hrelja <mail@kemoke.net>
2017-06-14 01:00:48 +0200

    fix socket

--- ChatServer/WebSockets/WebSocketHandler.cs
+++ ChatServer/WebSockets/WebSocketHandler.cs
@@ -30,8 +30,8 @@ namespace ChatServer.WebSockets
         {
             if (socket.State != WebSocketState.Open)
                 return;
-
-            await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.UTF8.GetBytes(message),
+            
+            await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.Unicode.GetBytes(message),
                     offset: 0,
                     count: message.Length),
                 messageType: WebSocketMessageType.Text,
